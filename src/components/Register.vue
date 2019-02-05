9db42a42873da9e09a7bce506e42b2fa07246c1a
John Doe <sanchezmorajeffrey@yahoo.com>
2019-02-04 18:27:06 -0600

    cambios

---
+++ src/components/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <auth-form action="register" v-on:process="register($event)"/>
+    <app-snack-bar v-if="snackBar" :snack-Bar="snackBar" :text="message" :timeout="timeout"/>-->
+    <auth-form action="register" v-on:process="register($event)"/>
+
+    <app-snack-bar v-if="snackBar" :snack-bar="snackBar" :text="message" :timeout="timeout"/>
+  </div>
+</template>
+
+<script>
+import AppSnackBar from "@/components/SnackBar";
+import AuthForm from "@/forms/auth";
+export default {
+  components: { AppSnackBar, AuthForm },
+  name: "Register",
+
+  data() {
+    return {
+      snackBar: false,
+      message: "",
+      timeout: 5000
+    };
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home';
-
+import Register from '@/components/register';
 
 Vue.use(Router)
 
@@ -12,6 +12,12 @@ export default new Router({
       name: 'Home',
       component: Home,
       meta: {Auth: false, title: 'Inicio'},
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: {Auth: false, title: 'Registro'},
     }
   ]
 })
